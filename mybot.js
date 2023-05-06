76d09d839bfeaca0d7f1309da2e9f136cda78301
metagamersuniversE <metagamersuniverse@gmail.com>
2023-05-07 01:33:46 +0600

    aa

--- mybot.js
+++ mybot.js
@@ -128,7 +128,7 @@ bot.onText(/\/start/, async (msg) => {
   const message = `
 <b>Hello! Welcome to the $LEPE Lottery bot.</b>
 
-To get help with using the bot, use the /help command.
+To get help with using the bot, use the /guide command.
 
 <i>You can also visit our website at https://www.luckypepe.io/ for more information and updates.</i>
 `;
