b959bfaf4ff682ee2961bba4c9f32b8f2ad0703c
metagamersuniversE <metagamersuniverse@gmail.com>
2023-05-09 22:33:19 +0600

    uuuu

--- lepe.js
+++ lepe.js
@@ -12,6 +12,121 @@ const contract = new ethers.Contract(contractAddress, contractABI, provider);
 // Create the Telegram bot
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true, debug: true });
 
+// Set up a timer to check the current round at regular intervals
+
+async function getDexscreenerData() {
+  const response = await axios.get('https://api.dexscreener.com/latest/dex/pairs/arbitrum/0xbf28fc1d36478c562ef25ab7701bd6f72f0d48b9,0xBF28FC1D36478C562ef25aB7701bd6f72f0d48B9');
+  const data = response.data;
+  const pair = data.pairs[0];
+  const price = pair.priceUsd;
+  const priceChange1h = pair.priceChange.h1;
+  const priceChange24h = pair.priceChange.h24;
+  const volume24h = pair.volume.h24;
+  const liquidity = pair.liquidity.usd;
+  const marketCap = pair.fdv;
+  const buyers24h = pair.txns.h24.buys;
+  const sellers24h = pair.txns.h24.sells;
+  return {
+    symbol: pair.baseToken.symbol,
+    name: pair.baseToken.name,
+    chainId: pair.chainId,
+    price: `$${price}`,
+    priceChange1h: `${priceChange1h.toFixed(2)}%`,
+    priceChange24h: `${priceChange24h.toFixed(2)}%`,
+    volume24h: `$${volume24h.toLocaleString()}`,
+    liquidity: `$${liquidity.toLocaleString()}`,
+    marketCap: `$${marketCap.toLocaleString()}`,
+    buyers24h: buyers24h,
+    sellers24h: sellers24h
+  };
+}
+
+//bot.onText(/\/price/, async (msg) => {
+  console.log('Price command received');
+  const data = await getDexscreenerData();
+  const message = `
+💰 ${data.symbol} Price: ${data.price}
+⚡ Name: ${data.name}
+⚡ Network: ${data.chainId}
+📈 1h: ${data.priceChange1h}
+📈 24h: ${data.priceChange24h}
+📊 Volume: ${data.volume24h}
+👥 24h Total Buyers: ${data.buyers24h}
+💦 Liquidity: ${data.liquidity}
+💎 Market Cap (FDV): ${data.marketCap}`;
+
+  const keyboard = {
+    inline_keyboard: [
+      [
+        { text: "📊Chart", url: "https://www.dexview.com/arbitrum/0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6" },
+        { text: "💰BUY NOW", url: "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6&chainId=42161" }
+      ]
+    ]
+  };
+
+  const options = {
+    reply_markup: JSON.stringify(keyboard),
+  };
+
+  bot.sendMessage(msg.chat.id, message, options);
+});
+
+
+// buy the /buy command
+bot.onText(/\/buy/, (msg) => {
+    console.log('ca command received');
+    const message = `
+  <b>To buy a $LEPE, please visit one of the following websites:</b>
+  `;
+    const options = {
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "📊Chart", url: "https://www.dexview.com/arbitrum/0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6" },
+            { text: "💰BUY NOW", url: "https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6&chainId=42161" }
+          ]
+        ]
+      }
+    };
+    bot.sendMessage(msg.chat.id, message, options);
+  });
+
+// Set up a timer to check the current round at regular intervals
+setInterval(async () => {
+    const currentRound = await contract._lotteryRound();
+    const previousRound = currentRound - 1;
+  
+    // Check if the previous round has ended
+    const hasRoundEnded = await contract.hasRoundEnded(previousRound);
+    if (hasRoundEnded) {
+      // Get the winner information for the previous round
+      const winnerInfo = await contract.lotteryWinnerInfo(previousRound);
+      const prizeAmount = isNaN(winnerInfo.prizeAmount) ? "0.0" : (winnerInfo.prizeAmount / 1e18).toFixed(5);
+      const arbAmount = isNaN(winnerInfo.arbAmount) ? "0.0" : (winnerInfo.arbAmount / 1e18).toFixed(5);
+      const message = `🎉 Round ${previousRound} of the $LEPE Lottery has ended! 🎉\n\nCongratulations to the winner:\nWin By Random Number: ${winnerInfo.randomNumber.toString()}\nWallet Address: ${winnerInfo.wallet}\nPrize Amount: ${arbAmount} ARB = ${prizeAmount} ETH`;
+  
+      // Send the winner information to all users in the chat
+      const chatId = process.env.CHAT_ID;
+      bot.sendMessage(chatId, message);
+      
+      // Check if the next round has started
+      if (currentRound > previousRound) {
+        const nextRound = currentRound;
+        const nextRoundMessage = `Round ${nextRound} has begun. Good luck to all participants! 🍀`;
+        bot.sendMessage(chatId, nextRoundMessage);
+      }
+    } else if (previousRound === 0) {
+      // Notify users that the lottery has started
+      const chatId = process.env.CHAT_ID;
+      const message = "The $LEPE Lottery has started! 🎉";
+      const sentMessage = await bot.sendMessage(chatId, message);
+  
+      // Pin the message to the chat
+      bot.pinChatMessage(chatId, sentMessage.message_id);
+    }
+  }, 60000); // Check every minute
+
 
 // Handle the /winner command
 bot.onText(/\/winner (.+)/, async (msg, match) => {
@@ -20,14 +135,9 @@ bot.onText(/\/winner (.+)/, async (msg, match) => {
   const winnerInfo = await contract.lotteryWinnerInfo(round);
   const prizeAmount = isNaN(winnerInfo.prizeAmount) ? "0.0" : (winnerInfo.prizeAmount / 1e18).toFixed(5);
   const arbAmount = isNaN(winnerInfo.arbAmount) ? "0.0" : (winnerInfo.arbAmount / 1e18).toFixed(5);
-  //const message = `Win By Random Number: ${winnerInfo.randomNumber.toString()}\nWallet Address: ${winnerInfo.wallet}\nPrize Amount: ${arbAmount} ARB = ${prizeAmount} ETH`;
-  const message = `
-<b>Get ready to win big!</b> 
-The lottery is coming soon, but it hasn't started yet. 
-Keep an eye out for updates and be prepared to participate when it opens.`
-  ;
-   bot.sendMessage(msg.chat.id, message, {parse_mode: "HTML"});
-  //bot.sendMessage(msg.chat.id, message);
+  const message = `Win By Random Number: ${winnerInfo.randomNumber.toString()}\nWallet Address: ${winnerInfo.wallet}\nPrize Amount: ${arbAmount} ARB = ${prizeAmount} ETH`;
+  
+  bot.sendMessage(msg.chat.id, message);
 });
 
 // Handle the /round command
@@ -35,14 +145,11 @@ bot.onText(/\/(round|lottery)/, async (msg) => {
   console.log('round/lottery command received'); // Add console.log() statement here
   const contractAddress = '0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6'; // Replace with actual contract address
   const round = await contract._lotteryRound(); // Call _lotteryRound function
-    //const message = `Current lottery round: ${round}`;
-    const message = `
-<b>Get ready to win big!</b> 
-The lottery is coming soon, but it hasn't started yet. 
-Keep an eye out for updates and be prepared to participate when it opens.`
-  ;
-   bot.sendMessage(msg.chat.id, message, {parse_mode: "HTML"});
-    //bot.sendMessage(msg.chat.id, message);
+    const message = `Current lottery round: ${round}`;
+    
+    bot.sendMessage(msg.chat.id, message);
+    // Pin the message to the chat
+      bot.pinChatMessage(chatId, sentMessage.message_id);
 });
 
 
@@ -68,14 +175,9 @@ bot.onText(/\/minimum/, async (msg) => {
   const contract = new ethers.Contract(contractAddress, contractABI, provider);
   const minAmount = await contract._minAmountToParticipate(); // Call _minAmountToParticipate function
     const minAmountInEther = ethers.utils.formatEther(minAmount);
-    //const message = `Minimum amount to participate in the lottery: ${minAmountInEther} ETH`;
-    const message = `
-<b>Get ready to win big!</b> 
-The lottery is coming soon, but it hasn't started yet. 
-Keep an eye out for updates and be prepared to participate when it opens.`
-  ;
-   bot.sendMessage(msg.chat.id, message, {parse_mode: "HTML"});
-    //bot.sendMessage(msg.chat.id, message);
+    const message = `Minimum amount to participate in the lottery: ${minAmountInEther} ETH`;
+    
+    bot.sendMessage(msg.chat.id, message);
 });
 
 // Handle the /balance command
@@ -84,14 +186,9 @@ bot.onText(/\/balance/, async (msg) => {
   const walletAddress = "0x087859e91ee03cb339ddd8df8e8f2a0b95fe07d6"; // replace with your desired wallet address
   const balance = await provider.getBalance(walletAddress);
   const formattedBalance = ethers.utils.formatEther(balance);
-  //const message = `Lottery Balance Amount: ${formattedBalance} ETH`;
-  const message = `
-<b>Get ready to win big!</b> 
-The lottery is coming soon, but it hasn't started yet. 
-Keep an eye out for updates and be prepared to participate when it opens.`
-  ;
-   bot.sendMessage(msg.chat.id, message, {parse_mode: "HTML"});
-                 //bot.sendMessage(msg.chat.id, message);
+  const message = `Lottery Balance Amount: ${formattedBalance} ETH`;
+  
+  bot.sendMessage(msg.chat.id, message);
   return; // Add return statement here to exit the function
 });
 
@@ -112,7 +209,6 @@ bot.onText(/\/(buy|ido|presale|pinksale)/, (msg) => {
   console.log('buy/ido/presale/pinksale command received');
   const message = `
 <b>To buy a $LEPE, please visit one of the following websites:</b>
-Presale is running..
 - Presale Start Time - 2023.05.09 15:00 (UTC)
 - Presale End Time - 2023.05.10 15:00 (UTC)
 `;
