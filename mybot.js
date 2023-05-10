37e1a1fc42fb5a1be3b5fda6e8eff08de4d0bc76
metagamersuniversE <metagamersuniverse@gmail.com>
2023-05-10 17:32:59 +0600

    aaaa

--- mybot.js
+++ mybot.js
@@ -20,14 +20,8 @@ bot.onText(/\/winner (.+)/, async (msg, match) => {
   const winnerInfo = await contract.lotteryWinnerInfo(round);
   const prizeAmount = isNaN(winnerInfo.prizeAmount) ? "0.0" : (winnerInfo.prizeAmount / 1e18).toFixed(5);
   const arbAmount = isNaN(winnerInfo.arbAmount) ? "0.0" : (winnerInfo.arbAmount / 1e18).toFixed(5);
-  //const message = `Win By Random Number: ${winnerInfo.randomNumber.toString()}\nWallet Address: ${winnerInfo.wallet}\nPrize Amount: ${arbAmount} ARB = ${prizeAmount} ETH`;
-  const message = `
-<b>Get ready to win big!</b> 
-The lottery is coming soon, but it hasn't started yet. 
-Keep an eye out for updates and be prepared to participate when it opens.`
-  ;
-   bot.sendMessage(msg.chat.id, message, {parse_mode: "HTML"});
-  //bot.sendMessage(msg.chat.id, message);
+  const message = `Win By Random Number: ${winnerInfo.randomNumber.toString()}\nWallet Address: ${winnerInfo.wallet}\nPrize Amount: ${arbAmount} ARB = ${prizeAmount} ETH`;
+  bot.sendMessage(msg.chat.id, message);
 });
 
 // Handle the /round command
@@ -35,14 +29,7 @@ bot.onText(/\/(round|lottery)/, async (msg) => {
   console.log('round/lottery command received'); // Add console.log() statement here
   const contractAddress = '0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6'; // Replace with actual contract address
   const round = await contract._lotteryRound(); // Call _lotteryRound function
-    //const message = `Current lottery round: ${round}`;
-    const message = `
-<b>Get ready to win big!</b> 
-The lottery is coming soon, but it hasn't started yet. 
-Keep an eye out for updates and be prepared to participate when it opens.`
-  ;
-   bot.sendMessage(msg.chat.id, message, {parse_mode: "HTML"});
-    //bot.sendMessage(msg.chat.id, message);
+  bot.sendMessage(msg.chat.id, message);
 });
 
 
@@ -106,14 +93,8 @@ bot.onText(/\/balance/, async (msg) => {
   const walletAddress = "0x087859e91ee03cb339ddd8df8e8f2a0b95fe07d6"; // replace with your desired wallet address
   const balance = await provider.getBalance(walletAddress);
   const formattedBalance = ethers.utils.formatEther(balance);
-  //const message = `Lottery Balance Amount: ${formattedBalance} ETH`;
-  const message = `
-<b>Get ready to win big!</b> 
-The lottery is coming soon, but it hasn't started yet. 
-Keep an eye out for updates and be prepared to participate when it opens.`
-  ;
-   bot.sendMessage(msg.chat.id, message, {parse_mode: "HTML"});
-                 //bot.sendMessage(msg.chat.id, message);
+  const message = `Lottery Balance Amount: ${formattedBalance} ETH`;
+  bot.sendMessage(msg.chat.id, message);
   return; // Add return statement here to exit the function
 });
 
@@ -163,6 +144,7 @@ bot.onText(/\/(buy|ido|presale|pinksale)/, (msg) => {
   };
   bot.sendMessage(msg.chat.id, message, options);
 });
+
 // Handle the /guide command
 bot.onText(/\/start/, async (msg) => {
   console.log('start command received'); // Add console.log() statement here
