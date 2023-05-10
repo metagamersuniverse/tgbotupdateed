c0f076a6bc40683f1c7a0cfabd73ea2cc49a97ae
metagamersuniversE <metagamersuniverse@gmail.com>
2023-05-10 18:37:54 +0600

    aaa

--- mybot.js
+++ mybot.js
@@ -89,28 +89,14 @@ bot.onText(/\/minimum/, async (msg) => {
   bot.sendMessage(msg.chat.id, message);
 });
 
-
-// Handle the /balance command
-bot.onText(/\/balance/, async (msg) => {
-  console.log('Balance command received'); // Add console.log() statement here
-  const walletAddress = "0x087859e91ee03cb339ddd8df8e8f2a0b95fe07d6"; // replace with your desired wallet address
-  const balance = await provider.getBalance(walletAddress);
-  const formattedBalance = ethers.utils.formatEther(balance);
-  const message = `Lottery Balance Amount: ${formattedBalance} ETH`;
-  bot.sendMessage(msg.chat.id, message);
-  return; // Add return statement here to exit the function
-});
-
 // Handle the /bido command
-bot.onText(/\/b/, async (msg) => {
-  console.log('bido command received');
+bot.onText(/\/balance/, async (msg) => {
+  console.log('balance command received');
 
   // Retrieve Ido balance in ETH
   const walletAddress = '0x087859e91ee03cb339ddd8df8e8f2a0b95fe07d6'; // Replace with your desired wallet address
   const balanceInWei = await provider.getBalance(walletAddress);
   const balanceInEth = ethers.utils.formatEther(balanceInWei);
-
-  
   // Retrieve ARB price from the API
   const pairAddress = '0xC6F780497A95e246EB9449f5e4770916DCd6396A';
   const apiEndpoint = `https://api.dexscreener.com/latest/dex/pairs/arbitrum/${pairAddress}`;
@@ -120,23 +106,31 @@ bot.onText(/\/b/, async (msg) => {
 
   // Calculate balance in ARB
   const balanceInArb = (balanceInEth / priceNative).toFixed(2);
-
-  const message = `Ido : ${balanceInEth} ETH = ${balanceInArb} ARB`;
+  const message = `Lottery Balance : ${balanceInEth} ETH = ${balanceInArb} ARB`;
   bot.sendMessage(msg.chat.id, message);
   return;
 });
 
-
-
 // Handle the /bido command
 bot.onText(/\/bido/, async (msg) => {
-  console.log('bido command received'); // Add console.log() statement here
-  const walletAddress = "0x0bcbbcd3186e5d857af2a4c4a158d5027037032f"; // replace with your desired wallet address
-  const balance = await provider.getBalance(walletAddress);
-  const formattedBalance = ethers.utils.formatEther(balance);
-  const message = `Ido : ${formattedBalance} ETH`;
+  console.log('balance command received');
+
+  // Retrieve Ido balance in ETH
+  const walletAddress = '0x0bcbbcd3186e5d857af2a4c4a158d5027037032f'; // Replace with your desired wallet address
+  const balanceInWei = await provider.getBalance(walletAddress);
+  const balanceInEth = ethers.utils.formatEther(balanceInWei);
+  // Retrieve ARB price from the API
+  const pairAddress = '0xC6F780497A95e246EB9449f5e4770916DCd6396A';
+  const apiEndpoint = `https://api.dexscreener.com/latest/dex/pairs/arbitrum/${pairAddress}`;
+  const response = await axios.get(apiEndpoint);
+  const data = response.data;
+  const priceNative = parseFloat(data.pairs[0].priceNative); // Get the price of the token in the trading pair
+
+  // Calculate balance in ARB
+  const balanceInArb = (balanceInEth / priceNative).toFixed(2);
+  const message = `Lottery Balance : ${balanceInEth} ETH = ${balanceInArb} ARB`;
   bot.sendMessage(msg.chat.id, message);
-  return; // Add return statement here to exit the function
+  return;
 });
 // Handle the /ca command
 bot.onText(/\/(ca|contract)/, async (msg) => {
