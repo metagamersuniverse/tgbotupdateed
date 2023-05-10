d99cfa707259415b827249eefa989efda97e100b
metagamersuniversE <metagamersuniverse@gmail.com>
2023-05-10 17:14:17 +0600

    aaa

--- mybot.js
+++ mybot.js
@@ -47,7 +47,7 @@ Keep an eye out for updates and be prepared to participate when it opens.`
 
 
 // Handle the /minAmount command
-bot.onText(/\/minimum/, async (msg) => {
+bot.onText(/\/minimume/, async (msg) => {
   console.log('minimum command received'); // Add console.log() statement here
   const contractAddress = '0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6'; // Replace with actual contract address
   const contractABI = [
@@ -73,7 +73,7 @@ bot.onText(/\/minimum/, async (msg) => {
 });
 
 // Handle the /minAmount command
-bot.onText(/\/minimumeth/, async (msg) => {
+bot.onText(/\/minimum/, async (msg) => {
   console.log('minimum command received'); // Add console.log() statement here
   const contractAddress = '0x6CB0e4dA8F621A3901573bD8c8d2C8A0987d78d6'; // Replace with actual contract address
   const contractABI = [
